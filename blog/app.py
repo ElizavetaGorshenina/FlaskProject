cd10c2e857f4cb3a96d74e0382871b2c1bcdc4f7
ElizavetaGorshenina <liza_summer@mail.ru>
2023-02-28 23:29:01 +0300

    lesson_4 Docker image with the project created; Postgres connected to the project

--- blog/app.py
+++ blog/app.py
@@ -3,13 +3,17 @@ from blog.views.users import users_app
 from blog.views.articles import articles_app
 from blog.models.database import db
 from blog.views.auth import login_manager, auth_app
+import os
+
 
 app = Flask(__name__)
+cfg_name = os.environ.get("CONFIG_NAME") or "BaseConfig"
+app.config.from_object(f"blog.configs.{cfg_name}")
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "abcdefg123456"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SECRET_KEY"] = "abcdefg123456"
 db.init_app(app)
 app.register_blueprint(auth_app, url_prefix="/auth")
 login_manager.init_app(app)

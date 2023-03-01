0f8051475470cb44cff1ac21af8a33ecbe6159fb
ElizavetaGorshenina <liza_summer@mail.ru>
2023-03-01 22:53:19 +0300

    lesson_4 User model updated, migrations for creation and update of User model created

--- blog/app.py
+++ blog/app.py
@@ -4,19 +4,18 @@ from blog.views.articles import articles_app
 from blog.models.database import db
 from blog.views.auth import login_manager, auth_app
 import os
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-cfg_name = os.environ.get("CONFIG_NAME") or "BaseConfig"
+cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
 app.config.from_object(f"blog.configs.{cfg_name}")
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.config["SECRET_KEY"] = "abcdefg123456"
 db.init_app(app)
 app.register_blueprint(auth_app, url_prefix="/auth")
 login_manager.init_app(app)
+migrate = Migrate(app, db, compare_type=True)
 
 
 @app.route("/")

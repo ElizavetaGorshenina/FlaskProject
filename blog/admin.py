a6830aeb8c8ef4171efb474b1247a2ec0ffd161a
ElizavetaGorshenina <liza_summer@mail.ru>
2023-03-14 23:24:07 +0300

    lesson_8 Article, Author, Tag and User models added to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,15 @@ from blog import models
 from blog.models.database import db
 
 
+# Customized admin interface
+class CustomView(ModelView):
+    pass
+
+
+# Create admin with custom base template
 admin = Admin(name="Blog Admin", template_mode="bootstrap4")
+# Add views
+admin.add_view(CustomView(models.Tag, db.session, category="Models"))
+admin.add_view(CustomView(models.Article, db.session, category="Models"))
+admin.add_view(CustomView(models.User, db.session, category="Models"))
+admin.add_view(CustomView(models.Author, db.session, category="Models"))

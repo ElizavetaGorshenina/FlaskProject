5abdd236de22d7b3568538e92275ab69c98984b5
ElizavetaGorshenina <liza_summer@mail.ru>
2023-03-18 02:44:15 +0300

    lesson_9 ApiSpecPlugin connected to the project

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -3,16 +3,38 @@ from blog.api.tag import TagList, TagDetail
 from blog.api.article import ArticleList, ArticleDetail
 from blog.api.author import AuthorList, AuthorDetail
 from blog.api.user import UserList, UserDetail
+from combojsonapi.spec import ApiSpecPlugin
+
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        # Declaring tags list with their descriptions,
+        # so API gets organized into groups. it's optional.
+        tags={
+            "Tag": "Tag API",
+            "User": "User API",
+            "Author": "Author API",
+            "Article": "Article API",
+        }
+    )
+    return api_spec_plugin
 
 
 def init_api(app):
-    api = Api(app)
-    api.route(TagList, "tag_list", "/api/tags/")
-    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
-    api.route(ArticleList, "article_list", "/api/articles/")
-    api.route(ArticleDetail, "article_detail", "/api/articles/<int:id>/")
-    api.route(AuthorList, "author_list", "/api/authors/")
-    api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/")
-    api.route(UserList, "user_list", "/api/users/")
-    api.route(UserDetail, "user_detail", "/api/users/<int:id>/")
+    api_spec_plugin = create_api_spec_plugin(app)
+    api = Api(
+        app,
+        plugins=[
+            api_spec_plugin,
+        ],
+    )
+    api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
+    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
+    api.route(ArticleList, "article_list", "/api/articles/", tag="Article")
+    api.route(ArticleDetail, "article_detail", "/api/articles/<int:id>/", tag="Article")
+    api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
+    api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/", tag="Author")
+    api.route(UserList, "user_list", "/api/users/", tag="User")
+    api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
     return api
